203c8e356332499c2e6289e8d64c48bee2130eaa
Olivier Roche <olivier.a.roche@gmail.com>
2018-10-26 12:27:11 +0200

    First push

---
+++ AlgPoly.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 30 23:59:23 2018
+
+@author: Augustine
+"""
+import numpy as np
+import random as rd
+
+def sort_tuple(t):
+    t_as_list = list(t)
+    t_as_list.sort()
+    return tuple(t_as_list)
+
+class Polynomial:
+#vars is the set of the index of all variables.
+#Say the variables are X_0,X_1,X_2. Then vars is the set {0,1,2}.
+
+#The coeff is a dictionary of coefficients, which keys are tuples. 
+#eg the key (0,1) in coeff corresponds to the monomial X_0X_1
+# the key (0,0,1) corresponds to the monomial X_0X_0X_1, ie X_0^2X_1 .
+# () corresponds to 1 (the constant term).
+# (0,) (resp. (1,)) corresponds to X_0 (resp. X_1).
+#To ensure that a given monomial only comes once in the keys of coeff,
+#the corresponding tuple is required to be ordered. eg, X_0^2X_1 might
+#be described either by (0,0,1), (1,0,0), (0,1,0) but only (0,0,1) is valid
+#since it is ordered. We use the function sort_tuple to achieve that.
+    def __init__(self,coeff = {}):  
+        self.coeff={}
+        if len(coeff.keys())==0:
+            self.coeff[()]= 0 
+        for k in coeff.keys():
+            self.coeff[sort_tuple(k)] = coeff[k]
+        self.vars=set([])
+        for c in self.coeff.keys():
+            if c!=():
+                for i in c:
+                    self.vars.add(i)
+                    
+    def evaluate(self,valuation):
+        #Evaluates the value of the plynomial for a given valuation.
+        #The valuation is given as a dictionary. 
+        #eg, say the polynomial is X_0^2+X_1, hence variables are X_0 and X_1,
+        #then evaluate({0: 4, 1: 13}) will return 4^2 + 13, ie 29.
+        
+        #First, check if the input is compatible :
+        if len(self.vars.symmetric_difference(valuation.keys())) != 0:
+            missing_vars = self.vars.difference(valuation.keys())
+            extra_vars = set(valuation.keys()).difference(self.vars)
+            message_fields = []
+            if len(missing_vars)!=0:
+                message_fields.append("variables {0} are missing \n".format(
+                                                            missing_vars))
+            if len(extra_vars)!=0:
+                message_fields.append("variables {0} were not expected".format(
+                                                                extra_vars))
+            message = ''.join(message_fields)
+            raise ValueError(message)
+        ret = 0
+        for monomial in self.coeff.keys():
+            term = self.coeff[monomial]
+            for var in monomial :
+                term *= valuation[var]
+            ret += term
+        return ret
+        
+    def raw_evaluate(self,array):
+        #This requires the variables of self to be range(len(array)).
+        return self.evaluate(raw_valuation(array))
+    
+    def __str__(self):
+        return str({k:self.coeff[k] for k in self.coeff.keys() if 
+                    self.coeff[k] !=0})
+    
+    def __add__(self,other):
+        sumcoeff=self.coeff.copy()
+        if isinstance(other,Polynomial):
+            for k in other.coeff.keys():
+                if k in sumcoeff.keys():
+                    sumcoeff[k]+=other.coeff[k]
+                else:
+                    sumcoeff[k]=other.coeff[k]
+        else:
+            #here, we assume that other is of numeric type
+            sumcoeff[()]+=other
+        return Polynomial(sumcoeff)
+    
+    def __radd__(self,other):
+        return self.__add__(other)
+    
+    def __mul__(self,other):
+        if not isinstance(other,Polynomial):
+            #again, we assume that other is then of numeric type
+            prodcoeff=self.coeff.copy()
+            for k in prodcoeff.keys():
+                prodcoeff[k]*=other
+        else:
+            prodcoeff={}
+            for sck in self.coeff.keys():
+                for ock in other.coeff.keys():
+                    r=self.coeff[sck]*other.coeff[ock]
+                    keyCtor=[]
+                    for v in self.vars | other.vars:
+                        for i in sck:
+                            if i==v:
+                                keyCtor.append(i)
+                        for i in ock:
+                            if i==v:
+                                keyCtor.append(i)
+                    newkey=tuple(keyCtor)
+                    if newkey in prodcoeff.keys():
+                        prodcoeff[newkey]+=r
+                    else:
+                        prodcoeff[newkey]=r
+        return Polynomial(prodcoeff)
+    
+    def __rmul__(self,other):
+        return self.__mul__(other)
+    
+    def __sub__(self,other):
+        return self+(-1)*other
+    
+    def __rsub__(self,other):
+        return other+(-1)*self
+    
+    def partial_derivative(self,var):
+        coeff_deriv={tuple([var]) : 0} #forces var to appear as a variable in 
+                                 # the returned polynomial 
+        for ck in self.coeff.keys():
+            t_deriv=self.coeff[ck]
+            deg_v=sum(i==var for i in ck)
+            t_deriv*=deg_v
+            keyCtor=[]
+            vNotMet=True
+            for i in ck:
+                if var==i and vNotMet:
+                    vNotMet=False
+                else:
+                    keyCtor.append(i)
+            newkey=tuple(keyCtor)
+            if newkey in coeff_deriv.keys():
+                coeff_deriv[newkey]+=t_deriv
+            else:
+                coeff_deriv[newkey]=t_deriv
+        return Polynomial(coeff_deriv)        
+    
+    def differential(self,valuation):
+       if len(set(valuation.keys()) ^ self.vars)!=0:
+           raise Exception("Incompatible Variables {0} expected, {1} given"
+                           .format(self.vars,valuation.keys()))
+       else:
+           return np.array([(self.partial_derivative(i)).evaluate(valuation) 
+                            for i in self.vars])
+
+#-----------------------------------------------------------------------------
+    
+"""
+--------help functions for arrays of Polynomials----------------------------
+
+in this section, poly_function refers to an array of instances of Polynomial
+whose variables indices range from 0 to n-1.
+Say poly_function is of shape (m,), poly_function must be thought as 
+ a function from R^n to R^m, which is polynomial componentwise.
+point is assumed to be an array of numbers of shape (n,), representing
+ a point in R^n."""
+    
+def evaluate_poly_func(poly_function,point):
+    return np.array([p.raw_evaluate(point) for p in poly_function])    
+    
+def differential(poly_function,point):
+    return np.array([p.differential(raw_valuation(point)) for p in poly_function])
+
+def initialize_polyfunc_zero(dim_input,dim_output,degree):
+    return np.array([fullZeroPolynomial(dim_input,degree) for i in range(dim_output)])
+
+
+def initialize_polyfunc_rand(dim_input,dim_output,degree):
+    return np.array([fullRandomPolynomial(dim_input,degree) for i in range(dim_output)])
+
+
+    #**********initializing functions***********************************
+    
+def iterTuple(nb_var,degree,to_yield = [],start = 0,empty_tuple = True):
+    #iters through all (ordered) tuples with values in range(nb_var-1) of
+    #degree less than degree in lexycographical order. eg iterTuple(2,3)
+    #iters trough [(), (0,), (0, 0), (0, 1), (0, 2), (1,), (1, 1), (1, 2), (2,), (2, 2)]
+    if empty_tuple:
+        yield ()
+    if len(to_yield)>=degree:
+        return
+    else:
+        for x in range(start,nb_var):
+            to_yield.append(x)
+            yield tuple(to_yield)
+            for res in iterTuple(nb_var,degree,to_yield,start=x,empty_tuple=False):
+                yield res
+            to_yield.pop()
+   
+def fullZeroPolynomial(nbVar,degree):
+    coeff = {}
+    for k in iterTuple(nbVar,degree):
+        coeff[k]=0.0
+    return Polynomial(coeff)
+
+def fullRandomPolynomial(nbVar,degree):
+    coeff = {}
+    for k in iterTuple(nbVar,degree):
+        coeff[k]=rd.random()
+    return Polynomial(coeff)
+
+    #***********************utilitaries**************************
+def raw_valuation(array):
+    #uses the values of array to build a valuation.
+    #eg raw_valuation(np.array([4,9,13])) gives {0: 4, 1: 9, 2: 13}
+    #that is, X_0 is evaluated as 4, X_1 as 9 and X_2 as 13
+    return {var : value for var,value in zip(range(len(array)),array)}
+
+def monom(tuple,array):
+    ret = 1
+    for i in tuple:
+        ret *= array[i]
+    return ret
---
+++ PolyNN.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 13:35:26 2018
+
+@author: Augustine
+"""
+
+import numpy as np
+
+import AlgPoly as ap
+
+def norme(x):
+    return np.sqrt(sum(x**2))
+
+def Id(n):
+      #the Identity matrix of R^n
+      return np.array([[1*(i==j) for j in range(n)]for i in range (n)])
+
+class ComponentwiseC1Function:
+    def __init__(self,main,derivative):
+        self.main=main
+        self.derivative = derivative
+        self.differential = lambda array : Id(len(array))*derivative(array)
+        
+    
+#*********Activation functions*****************
+    
+sigm=lambda x:1/(1+np.exp(-x))
+sigmP=lambda x:sigm(x)*(1-sigm(x))
+sigmoid=ComponentwiseC1Function(sigm,sigmP)
+
+relu=ComponentwiseC1Function(lambda x:x*(x>0),lambda x:1*(x>0))
+
+identity=ComponentwiseC1Function(lambda x:x,lambda x:1)
+
+step=ComponentwiseC1Function(lambda x:(-1.0*(x<-1)+x*(np.abs(x)<=1)+(x>1)), lambda x:(0.0*(x<1)+1*(np.abs(x)<=1)+0.0*(x>1)))
+
+#*******************************************
+
+def nabla_cost(a,y):
+    return a-y
+
+class PolyNN:
+    def __init__(self,layersSizes,thresholdFunction,degrees,damping = None):
+        self.numberLayers = len(layersSizes)
+        self.layersSizes = layersSizes
+        self.degrees = degrees
+        self.functions = [ap.initialize_polyfunc_rand(inp,outp,deg)
+            for inp,outp,deg in zip(layersSizes[:-1],layersSizes[1:],degrees)]
+        self.thresh=thresholdFunction
+        self.damping = damping
+    def feedForward(self,x):
+        activation = x
+        for t in self.functions:
+            z = ap.evaluate_poly_func(t,activation)
+            activation=self.thresh.main(z)
+        return activation
+
+    def backPropagation(self,x,y):
+        # ******forward pass*****
+        activations=[]
+        zValues=[]
+        activation=x
+        activations.append(activation)
+        for t in self.functions:
+            z = ap.evaluate_poly_func(t,activation)
+            zValues.append(z)
+            activation = self.thresh.main(z)
+            activations.append(activation)
+        #*********initialisation for backward pass***********************
+        #delta_P will be our return value. It is a convenient way
+        #to store the partial derivatives of cost with respect to the
+        #different parameters of self.functions .
+        delta_P = [ap.initialize_polyfunc_zero(inp,outp,deg) for inp,outp,deg  
+                   in zip(self.layersSizes[:-1],self.layersSizes[1:],self.degrees)]
+        #damping is a trick to avoid explosion of our return value
+        if self.damping is not None:
+            nabla_Cost_wrt_a = nabla_cost(activation,y)/(self.damping*norme(x))
+        else:
+            nabla_Cost_wrt_a = nabla_cost(activation,y)
+        
+        #*****backward passes*****
+        for l in range(self.numberLayers-2,-1,-1):
+            for i in range(self.layersSizes[l+1]):
+                delta_P[l][i].coeff = {tuple : self.thresh.derivative(zValues[l][i]) 
+                       * ap.monom(tuple,activations[l]) 
+                       * nabla_Cost_wrt_a[i] 
+                       for tuple in ap.iterTuple(nb_var=self.layersSizes[l],
+                                             degree=self.degrees[l])
+                                        }
+            nabla_Cost_wrt_a = np.dot(np.dot(self.thresh.differential(zValues[l]),\
+                ap.differential(self.functions[l],activations[l])).transpose(),
+                nabla_Cost_wrt_a)
+        return delta_P
+    
+    def steplearning(self,x,y,eta):
+        """ modifies self.functions using self.backPropagation(x,y)
+        eta is the change rate """     
+        delta_P = self.backPropagation(x,y)
+        for l in range(self.numberLayers-1):
+            self.functions[l] -= eta* delta_P[l]           
+#    def backPropagation(self,x,y):
+#        ###forward pass###
+#        activations=[]
+#        a=x.reshape(self.layersSizes[0],1)
+#        activations.append(a)
+#        zValues=[]
+#        for w,b,c in zip(self.weights,self.biases,self.XY):
+#            z=np.dot(w,a)+b+np.dot(c,productsVector(a))
+#            zValues.append(z)
+#            a=self.thresh(z)
+#            activations.append(a)
+#        ###initialisation for backward pass###
+#        delta_b = [np.zeros(b.shape) for b in self.biases]
+#        delta_w = [np.zeros(w.shape) for w in self.weights]
+#        delta_c=[np.zeros(c.shape) for c in self.XY]
+#        nabla_Cost=costDerivative(a,y.reshape(self.layersSizes[-1],1))/(self.damping*norme(x))
+#        delta_b[-1]=nabla_Cost
+#        delta_w[-1]=np.dot(nabla_Cost,activations[-2].transpose())
+#        nbXY=self.XY[-1].shape[1]
+#        deriv_XY=np.array([[activations[-2][pRead(ind,self.layersSizes[-2])[0]]*activations[-2][pRead(ind,self.layersSizes[-2])[1]] for ind in range(nbXY)]for j in range(self.layersSizes[-1])])
+#        delta_c[-1]=nabla_Cost*deriv_XY[:,:,0]
+#        ###backward passes###
+#        for l in range(self.numberLayers-3,-1,-1):
+#            nabla_Cost=np.dot((self.differentialTrans(activations[l+1],l+1)).transpose(),nabla_Cost)*self.threshDerivative(zValues[l])
+##            nabla_Cost=np.dot(self.weights[l+1].transpose(),nabla_Cost)*self.threshDerivative(zValues[l])
+#            delta_b[l]=nabla_Cost
+#            delta_w[l]=np.dot(nabla_Cost,activations[l].transpose())
+#            delta_c[l]=self.differentialCross(activations[l],l)*nabla_Cost
+#        return delta_b,delta_w,delta_c
+#    def stepLearning(self,x,y,eta):
+#        nb,nw,nc=self.backPropagation(x,y)        
+#        for l in range(self.numberLayers-1):
+#            self.biases[l]-=eta*nb[l]
+#            self.weights[l]-=eta*nw[l]
+#            self.XY[l]-=eta*nc[l]
+#    def differentialCross(self,a,l):
+#        sizeXY=self.layersSizes[l]*(self.layersSizes[l]+1)/2
+#        size=self.layersSizes[l+1]
+#        derivprod=[[0.0 for ind in range(sizeXY)] for i in range(size)]
+#        for i in range(size):
+#            for ind in range(sizeXY):
+#                j,k=pRead(ind,self.layersSizes[l])
+#                if j==k:
+#                    derivprod[i][ind]=2*a[j]
+#                else:
+#                    derivprod[i][ind]=a[j]*a[k]
+#        return np.array(derivprod)[:,:,0]
+#    def differentialTrans(self,a,l):
+#        n=self.layersSizes[l]
+#        diff=self.weights[l]
+#        for j in range(self.layersSizes[l+1]):
+#            for ind in range(n*(n+1)/2):
+#                o,p=pRead(ind,n)
+#                if o==j:
+#                    diff[j,o]+=self.XY[l][j,ind]*a[p]
+#                if p==j:
+#                    diff[j,p]+=self.XY[l][j,ind]*a[o]                    
+#        return diff
+#    def batchLearning(self,batch,eta):
+#        db=[np.zeros(b.shape) for b in self.biases]
+#        dw=[np.zeros(w.shape) for w in self.weights]
+#        dc=[np.zeros(c.shape) for c in self.XY]
+#        for x,y in batch:
+#            nb,nw,nc=self.backPropagation(x,y)
+#            for l in range(self.numberLayers -1):
+#                db[l]+=nb[l]
+#                dw[l]+=nw[l]
+#                dc[l]+=nc[l]
+#        for l in range(self.numberLayers-1):
+#            self.biases[l]-=eta*db[l]
+#            self.weights[l]-=eta*dw[l]
+#            self.XY[l]-=eta*dc[l]
